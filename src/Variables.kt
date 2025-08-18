ad8f428ab4a6fc7232ba230199501831c3b0cee7
dibya-dhal <dibyadhal32@gmail.com>
2025-08-18 13:16:18 +0530

    Add Variable.kt

---
+++ src/Variables.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var name: String = "John" // var is mutable
+    val age : Int = 25// val is immutable
+
+    println("Name : $name")
+    println("Age : $age")
+
+    name = "Rohan" // so var can be changed
+    println("Updated Name : $name")
+}
