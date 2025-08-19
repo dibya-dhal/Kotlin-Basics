a4eae0f980b66aff09ba96b3a15c85278040a318
dibya-dhal <dibyadhal32@gmail.com>
2025-08-19 11:52:28 +0530

    Day 2 - Added Data types examples (String,Int,Char,Boolean,Double

---
+++ src/DataTypes.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    val name  = "John" // String data type -> used for storing text
+    println("Name ; $name")
+
+    val number  = 12   // Int data type -> used for storing whole numbers
+    println("Number : $number")
+
+    val grade ='U'   // Char data type -> used for storing single characters
+    println("Letter : $grade")
+
+    val isAndroidFun  = true  // Boolean data type-> used for true/false value
+    println("Is a Boy : $isAndroidFun")
+
+    val height = 5.10    // Double data type -> used for storing decimal numbers
+    println("Height : $height")
+
+
+
+}
