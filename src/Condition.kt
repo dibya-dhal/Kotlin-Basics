c79774d098a3667815f7d39dccde18a389d34e91
dibya-dhal <dibyadhal32@gmail.com>
2025-08-22 23:15:02 +0530

    Added if-else ladder and when Condition examples

--- src/Condition.kt
+++ src/Condition.kt
@@ -14,8 +14,27 @@ fun main() {
         println("$number is Odd")
     }
 
+// if else if ladder
+    if (number < 0) {
+        println("Number is Negative")
+    }else if (number == 0){
+        println("Number is Zero")
+    }else {
+        println("Number is Positive")
+    }
 
-
+ // when statement
+    val day = 5
+    when(day) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Invalid day")
+    }
 
 
 
