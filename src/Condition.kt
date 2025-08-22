4110aff708bcf5ee51df5dab53a70e6edf299aa2
dibya-dhal <dibyadhal32@gmail.com>
2025-08-22 22:47:40 +0530

    Day 4 : Practice Condition Statement

---
+++ src/Condition.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val number = 15
+
+    // Simple if Condition
+    if (number > 10){
+        println("$number is greater than 10")
+    }
+
+    // if-else Condition
+    if (number % 2 == 0){
+        println("$number is Even")
+    }else{
+        println("$number is Odd")
+    }
+
+}
