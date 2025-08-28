361e50325a0e78674ff0cc49989703481c919a04
dibya-dhal <dibyadhal32@gmail.com>
2025-08-28 22:51:08 +0530

    Day 10: Practice interface example with Vehicle interface

--- src/Interface.kt
+++ src/Interface.kt
@@ -1,7 +1,19 @@
-interface Animal1{
-    val name: String
-    fun sound() // abstract methos
-    fun info(){
-        println("Animal info : $name")
+interface Vehicle{
+    fun start()
+    fun stop() {
+        println("Stopping vehicle...")
     }
+}
+
+class Car : Vehicle {
+    override fun start() {
+        println("Car is starting..")
+    }
+}
+
+fun main() {
+
+    val car = Car()
+    car.start()
+    car.stop()
 }
