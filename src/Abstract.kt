3e428d2fbd5d2b43234b8f068d49caa6be0614ab
dibya-dhal <dibyadhal32@gmail.com>
2025-08-29 22:27:05 +0530

    Day 11: Practice abstract class with different types of example

--- src/Abstract.kt
+++ src/Abstract.kt
@@ -1,7 +1,29 @@
-abstract class Vehicle1{
-
-    abstract fun start()
-    fun stop(){
-        println("Vehicle stopped..")
+//Abstract class
+abstract class Animal1(val name: String){
+ // abstract function
+    abstract fun makeSound()
+    fun sleep(){
+        println("$name is sleeping..")
     }
+}
+
+// Child class Dog
+class Dog1(name: String) : Animal1(name){
+    override fun makeSound() {
+        println("$name says Woof Woof!")
+    }
+}
+//Child class Cat
+class Cat(name: String):Animal1(name){
+    override fun makeSound() {
+        println("$name says : Meow Meow!..")
+    }
+}
+
+fun main() {
+    val dog = Dog1("Tommy")
+    val cat = Cat("Kitty")
+    dog.makeSound()
+    cat.makeSound()
+
 }
