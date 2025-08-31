adbff84a690a905e9cfd3db62df6afd6a7e56c98
dibya-dhal <dibyadhal32@gmail.com>
2025-08-31 22:12:33 +0530

    Day 13: Practice protected example in Encapsulation

--- src/Protected.kt
+++ src/Protected.kt
@@ -1,3 +1,20 @@
-class Parent1 {
+open class Parent2 {
     protected var secret = "family secret"
+    protected fun tellSecret() {
+        println("Telling secret : $secret")
+    }
+}
+class Child: Parent2(){
+    fun useSecret(){
+        println(secret)
+        tellSecret()
+    }
+}
+
+fun main() {
+
+    val c = Child()
+    c.useSecret()
+
+    //println(c.secret)  (it's show error)
 }
