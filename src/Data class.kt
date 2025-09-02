52da8ea737138a746569762f8b0c03e1a15c28c2
dibya-dhal <dibyadhal32@gmail.com>
2025-09-02 22:09:21 +0530

    Day 15: Practice Data Class Example

--- src/Data class.kt
+++ src/Data class.kt
@@ -1,1 +1,6 @@
-data class User(val name: String, val age : Int)
+data class Student1(val name: String, val age : Int)
+
+fun main() {
+    val student = Student1("John Snow",20)
+    println(student)
+}
