6ed0fe5f4f0bd25efe7d2bcc8a04278491ed2daa
dibya-dhal <dibyadhal32@gmail.com>
2025-09-02 21:46:09 +0530

    Day 14: Practice internal example in Encapsulation

--- src/internal.kt
+++ src/internal.kt
@@ -1,12 +1,20 @@
-internal class Library {
-    internal fun message() {
-        println("This is internal message")
+internal class Employee(private val name: String, private val salary: Int) {
+    // internal function
+    internal fun show() {
+        println("Employee Name : $name, Salary : $salary")
     }
 
 }
 
+//another internal class
+internal class Department(private val deptName: String){
+    internal fun showDept(){
+        println("Department: $deptName")
+    }
+}
+
 fun main() {
-    val lib = Library()
-    lib.message()
 
 }
+
+
