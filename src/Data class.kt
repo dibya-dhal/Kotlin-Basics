ab39681f1d155715abd49847169af07ccc6ebbd7
dibya-dhal <dibyadhal32@gmail.com>
2025-09-03 22:19:35 +0530

    Day 16: Practice Data Class Example with copy, equals

--- src/Data class.kt
+++ src/Data class.kt
@@ -1,6 +1,8 @@
-data class Student1(val name: String, val age : Int)
+data class Student1(val name: String, val age : Int,val course : String)
 
 fun main() {
-    val student = Student1("John Snow",20)
-    println(student)
+    val student1 = Student1("John Snow",20,"Computer Science")
+    val student2 = Student("Rohit",21,"Mathematics")
+    println(student1)
+    println(student2)
 }
