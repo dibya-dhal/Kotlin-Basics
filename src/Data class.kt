6633f89f1f6789cdeed0804530feaf61475bf9ca
dibya-dhal <dibyadhal32@gmail.com>
2025-09-03 22:30:08 +0530

    Day 16: Practice Data Class Example with copy, equals

--- src/Data class.kt
+++ src/Data class.kt
@@ -2,7 +2,13 @@ data class Student1(val name: String, val age : Int,val course : String)
 
 fun main() {
     val student1 = Student1("John Snow",20,"Computer Science")
-    val student2 = Student("Rohit",21,"Mathematics")
-    println(student1)
-    println(student2)
+    val student2 = Student1("Rohit",21,"Mathematics")
+    val student3 = Student1("John Snow",20,"Computer Science")
+
+    // toString() auto-generated
+    println("Student1 : $student1")
+    println("Student2 : $student2")
+
+    // equals()
+    println("Is student1 equal to student3 : ${student1 == student3}")
 }
