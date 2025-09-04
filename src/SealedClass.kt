cbb23420d50a20b9f12397ed365cedd090f75a5c
dibya-dhal <dibyadhal32@gmail.com>
2025-09-04 22:45:56 +0530

    Day 17: Added sealed class example with Success , Error and Loading states

--- src/SealedClass.kt
+++ src/SealedClass.kt
@@ -2,3 +2,18 @@ sealed class Result
 
 data class Success(val data : String) : Result()
 data class Error(val message : String): Result()
+
+fun handleResult(result: Result) {
+    when(result) {
+        is Success -> println("Success : ${result.data}")
+        is Error -> println("Error : ${result.message}")
+    }
+}
+fun main() {
+
+    val r1 : Result = Success("Data Loaded..")
+    val r2 : Result = Error("Network Error")
+
+    handleResult(r1)
+    handleResult(r2)
+}
