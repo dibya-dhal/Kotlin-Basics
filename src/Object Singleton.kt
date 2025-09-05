69613028ef8bde736717936b89ffa7832fdab44f
dibya-dhal <dibyadhal32@gmail.com>
2025-09-05 22:52:25 +0530

    Day 18: Added object singleton example (AppConfig)

--- src/Object Singleton.kt
+++ src/Object Singleton.kt
@@ -3,6 +3,18 @@ object AppConfig{
     val appName: String = "My KotlinApp"
     var version : String = "1.0.0"
 
+    fun printConfig() {
+        println("App Name : $appName , Version : $version")
+    }
 
+}
+
+fun main() {
+
+    AppConfig.printConfig()
+
+
+    AppConfig.version= "1. 1. 0"
+     AppConfig.printConfig()
 
 }
