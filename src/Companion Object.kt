5daeb078cba5e8503ad9f0552828eb53175e7dca
dibya-dhal <dibyadhal32@gmail.com>
2025-09-07 23:00:44 +0530

    Day 20: Practice Companion Object example with factory method

--- src/Companion Object.kt
+++ src/Companion Object.kt
@@ -4,5 +4,19 @@ class User private constructor(val name : String) {
         fun createGuest() : User{
             return User("Guest")
         }
+
+        fun createWithName(name : String) : User {
+            return User(name)
+        }
     }
+}
+
+fun main() {
+
+    val guest = User.createGuest()
+    println("User : ${guest.name}")
+
+    val Rohan = User.createWithName("Rohan")
+    println("User : ${Rohan.name}")
+
 }
