b394c5020b5a838bb47ce2c59a1fd5b47ad3823f
dibya-dhal <dibyadhal32@gmail.com>
2025-09-08 23:20:41 +0530

    Day 21: Practice Enum class example

--- src/Enum Class.kt
+++ src/Enum Class.kt
@@ -3,3 +3,18 @@ enum class Direction{
     SOUTH,
     EAST,WEST
 }
+
+fun move(direction: Direction){
+    when(direction){
+        Direction.NORTH ->
+            println("Moving up..")
+        Direction.SOUTH ->
+            println("Moving down..")
+        Direction.EAST ->
+            println("Moving right..")
+        Direction.WEST ->
+            println("Moving left..")
+
+
+    }
+}
