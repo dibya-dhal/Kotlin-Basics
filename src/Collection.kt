ae9134395823095f2f4f9d8935edf50dd905af25
dibya-dhal <dibyadhal32@gmail.com>
2025-09-11 22:31:15 +0530

    Day 23: Practice Kotlin List Collection

--- src/Collection.kt
+++ src/Collection.kt
@@ -3,4 +3,6 @@ fun main() {
     val fruits = listOf("Apple","Orange","Mango")
 
     println("Fruits List : $fruits")
+    println("First fruit : ${fruits[0]}")
+    println("Total fruits : ${fruits.size}")
 }
