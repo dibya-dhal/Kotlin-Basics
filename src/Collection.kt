3a98b31b0bfc31b1d1f46f7dabfbf180b71ee902
dibya-dhal <dibyadhal32@gmail.com>
2025-09-12 22:22:29 +0530

    Day 24: Practice Kotlin MutableList collection

--- src/Collection.kt
+++ src/Collection.kt
@@ -7,10 +7,17 @@ fun main() {
     println("Total fruits : ${fruits.size}")
     println()
 
+    //MutableList
     val numbers = mutableListOf(1,2,3)
 
     println("Original List : $numbers")
     numbers.add(4)
     numbers.add(5)
     println("Updated List : $numbers")
+    numbers.remove(2)
+    numbers[0] = 10
+
+    for (num in numbers){
+        println("Number : $num")
+    }
 }
