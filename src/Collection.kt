6a8c8a4c627b02d84b30fbffd2ec76b40831ab99
dibya-dhal <dibyadhal32@gmail.com>
2025-09-12 22:17:50 +0530

    Day 24: Practice Kotlin MutableList collection

--- src/Collection.kt
+++ src/Collection.kt
@@ -5,4 +5,9 @@ fun main() {
     println("Fruits List : $fruits")
     println("First fruit : ${fruits[0]}")
     println("Total fruits : ${fruits.size}")
+    println()
+
+    val numbers = mutableListOf(1,2,3)
+
+    println("Original List : $numbers")
 }
