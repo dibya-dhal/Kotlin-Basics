f2b373a3993e41ff744daab9029575e054be1a24
dibya-dhal <dibyadhal32@gmail.com>
2025-09-13 22:31:54 +0530

    Day 25: Practice Kotlin Set collection

--- src/CollectionSet.kt
+++ src/CollectionSet.kt
@@ -1,9 +1,16 @@
 fun main() {
 
 
-    val numbers = setOf(1,2,3,2,4,5,5,6)
+    val numbers = setOf(1,2,3,2,4,5,5,6) // duplicate are ignored
 
     println("Numbers Set : $numbers")
     println("Contains 3? ${numbers.contains(3)}")
+
+    println("Total unique numbers : ${numbers.size}")
+
+    //Loop through set
+    for (num  in numbers){
+        println(num)
+    }
 
 }
