2d5e3d5f54f1d5a568a267a96c977617a252d69d
dibya-dhal <dibyadhal32@gmail.com>
2025-09-14 21:23:38 +0530

    Day 26: Practice Kotlin Map collection

--- src/CollectionMap.kt
+++ src/CollectionMap.kt
@@ -2,4 +2,9 @@ fun main() {
 
     val studentMarks = mapOf(1 to 85, 2 to 90 , 3 to 95)
 
+    println("Student Marks : $studentMarks")
+
+    // Access value by key
+    println("Marks of roll number 2 : ${studentMarks[2]}")
+
 }
