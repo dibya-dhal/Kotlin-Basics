73a55feedc8d3ba6f0983af5ecc5b64d497c3356
dibya-dhal <dibyadhal32@gmail.com>
2025-09-16 21:56:25 +0530

    Day 28: Update higher-order functions with lambda

--- src/Lambda.kt
+++ src/Lambda.kt
@@ -1,3 +1,9 @@
 fun calculate(a : Int , b : Int , operation : (Int, Int) -> Int) : Int{
     return operation(a,b)
 }
+
+fun main() {
+    val sum = calculate(5,3){x,y -> x + y}
+    println("Sum : $sum")
+
+}
