59063bc19eacab0d675160c4a74d4fab88dce60d
dibya-dhal <dibyadhal32@gmail.com>
2025-09-18 22:17:36 +0530

    Day 30: Practice extension function for String and Int

--- src/ExtensionFun.kt
+++ src/ExtensionFun.kt
@@ -1,3 +1,13 @@
 fun String.addExclamation() : String {
     return this + "!"
 }
+
+fun Int.square() : Int{
+    return this * this
+}
+
+fun main() {
+    val text = "Hello".addExclamation()
+    println(text)
+
+}
