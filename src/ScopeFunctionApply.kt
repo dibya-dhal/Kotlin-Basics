78ab88ffcd8f4837dae0222daf8bc7d8590e531a
dibya-dhal <dibyadhal32@gmail.com>
2025-09-20 22:05:24 +0530

    Day 33 : Practice Scope function with apply example with data class

--- src/ScopeFunctionApply.kt
+++ src/ScopeFunctionApply.kt
@@ -2,6 +2,12 @@ data class Person2(var name : String, var age : Int )
 
 fun main() {
 
-    val person = Person2("Unknown",0)
+    val person = Person2("Unknown",0).apply {
+        name = "Alice"
+        age = 21
+
+    }
+
+    println("Person Details : $person")
 
 }
