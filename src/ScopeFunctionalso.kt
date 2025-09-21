968e88f496847e8cba978b2cd698a37e9656a085
dibya-dhal <dibyadhal32@gmail.com>
2025-09-21 22:08:57 +0530

    Day 34 : Practice Scope function with also

--- src/ScopeFunctionalso.kt
+++ src/ScopeFunctionalso.kt
@@ -2,5 +2,12 @@ data class Bike(var brand : String , var price: Int)
 
 fun main() {
 
+val bike = Bike("Unknown",0).also {
+    it.brand= "Royal Enfield"
+    it.price= 200000
+    println("Inside also : $it")
+}
+
+    println("Outside also: $bike")
 
 }
