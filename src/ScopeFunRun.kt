49811756d05347a52ea4ef412b6865b1590915d8
dibya-dhal <dibyadhal32@gmail.com>
2025-09-22 22:17:07 +0530

    Day 35 : Practice Scope function run example with Laptop data class

--- src/ScopeFunRun.kt
+++ src/ScopeFunRun.kt
@@ -1,6 +1,14 @@
-data class Laptop(val brand : String, val price : Int)
+data class Laptop(var brand : String, var price : Int)
 
 fun main() {
+
+val laptop = Laptop("Unknown",0).run {
+    brand = "acer"
+    price = 50000
+    this
+}
+
+    println("Laptop details : $laptop")
 
 
 }
