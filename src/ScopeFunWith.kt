03fa563f620e511877f63c5b85a3a83c6334155c
dibya-dhal <dibyadhal32@gmail.com>
2025-09-23 22:00:46 +0530

    Day 36 : Practice Scope function With example using Book data class

--- src/ScopeFunWith.kt
+++ src/ScopeFunWith.kt
@@ -3,5 +3,15 @@ data class Book(var title : String, var author : String )
 fun main() {
 
 
+    val book = Book("Unknown","Unknown")
+
+    val description = with(book){
+        title = "Kotlin for Beginners"
+        author = "John Doe"
+        "Book : $title by $author  "
+    }
+
+
+    println(description)
 
 }
