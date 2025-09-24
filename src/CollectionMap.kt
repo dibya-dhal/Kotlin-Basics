22b7e1b67d953829721c5b4d97f823859786eb95
dibya-dhal <dibyadhal32@gmail.com>
2025-09-24 22:22:27 +0530

    Day 37 : Practice Collection function map example with number and String

--- src/CollectionMap.kt
+++ src/CollectionMap.kt
@@ -6,11 +6,16 @@ fun main() {
 
     // Access value by key
     println("Marks of roll number 2 : ${studentMarks[2]}")
+    println()
 
 
 
     val number = listOf<Int>(2,4,6,8)
     val square = number.map { it * it }
     println("Squares : $square")
+
+    val names = listOf<String>("alice","rohan","charlie")
+    val capitalized =names.map { it.replaceFirstChar { c -> c.uppercase()} }
+    println("Capitalized : $capitalized")
 
 }
