0875b1013e085bbe9eba74ae3eeca0a8d06f91ab
dibya-dhal <dibyadhal32@gmail.com>
2025-09-30 20:51:23 +0530

    Day 42 : Practice not-null assertion operator (!!) in kotlin

--- src/Not-Null Assertion.kt
+++ src/Not-Null Assertion.kt
@@ -5,5 +5,9 @@ fun main() {
     val length = name!!.length
     println("Length of name : $length")
 
+    // if value is null, it will throw NullPointerException
+
+    val nickname: String? = null
+    // println(nickname!!.length)// This will crash the program
 
 }
