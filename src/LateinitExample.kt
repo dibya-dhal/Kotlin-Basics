5e37d7a3a3ce142d40c4de484dbdcfa46be7a370
dibya-dhal <dibyadhal32@gmail.com>
2025-10-01 22:12:40 +0530

    Day 43 : Practice lateinit variable in kotlin

--- src/LateinitExample.kt
+++ src/LateinitExample.kt
@@ -1,9 +1,26 @@
 class User1 {
 
-    lateinit var name : String
+    lateinit var name1 : String
 
     fun setName(newName : String){
-        name = newName
+        name1 = newName
     }
 
+    fun printName() {
+        if (::name1.isInitialized){
+            println("User name is : $name1")
+        }else {
+            println("Name is not initialized yet")
+        }
+    }
+
+}
+
+fun main() {
+
+    val user = User1()
+    user.printName()
+
+    user.setName("Rohan")
+    user.printName()
 }
