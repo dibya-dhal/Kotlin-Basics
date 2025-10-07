fd382102025cb8cb7bacc224b44dae4d81461844
dibya-dhal <dibyadhal32@gmail.com>
2025-10-07 21:46:48 +0530

    Day 46 : Update generics in kotlin

--- src/Generics.kt
+++ src/Generics.kt
@@ -4,10 +4,16 @@ class Box<T> (val content : T){
     }
 }
 
+fun <T> printTwice(value : T){
+    println(value)
+    println(value)
+}
+
 fun main() {
     val intBox = Box(123)
+    intBox.displayContent()
     val stringBox = Box("Hello Kotlin")
-
+stringBox.displayContent()
 
     println(intBox.displayContent())
     println(stringBox.displayContent())
