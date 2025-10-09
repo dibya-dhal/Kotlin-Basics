40cfc8e40a86f8f5a66e0598ba0b15d1c24e78be
dibya-dhal <dibyadhal32@gmail.com>
2025-10-09 21:54:31 +0530

    Day 48 : Practice Custom exception handling in kotlin

--- src/CustomException.kt
+++ src/CustomException.kt
@@ -2,5 +2,19 @@ class InvalidAgeException(message: String) : Exception(message)
 fun checkAge(age: Int){
     if (age < 18){
         throw InvalidAgeException("Age must be 18 or above")
+    }else {
+        println("Access granted : Age: $age")
     }
+}
+
+fun main() {
+
+    try {
+        checkAge(15)
+    }catch (e: InvalidAgeException){
+        println("Custom Exception Caught : ${e.message}")
+    }finally {
+        println("Check completed")
+    }
+
 }
