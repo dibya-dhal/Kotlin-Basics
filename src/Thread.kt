2f0708df478e3779c31f0129477264dc7995a250
dibya-dhal <dibyadhal32@gmail.com>
2025-10-10 21:58:10 +0530

    Day 49 : Practice multithreading in Kotlin using Thread class

--- src/Thread.kt
+++ src/Thread.kt
@@ -2,4 +2,17 @@ fun main() {
 
     println("Main Thread starts : ${Thread.currentThread().name}")
 
+    val thread = Thread{
+        for (i in 1..5){
+            println("Child Thread : $i")
+
+            Thread.sleep(500)
+        }
+    }
+
+    thread.start()
+    thread.join()
+
+    println("Main thread ends : ${Thread.currentThread().name}")
+
 }
