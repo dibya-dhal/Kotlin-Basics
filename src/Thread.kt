4fffbe6a1f41ffbfdccb553e53290b567b3d923d
dibya-dhal <dibyadhal32@gmail.com>
2025-10-11 22:07:25 +0530

    Day 45 : Practice multithreading in Kotlin using Runnable interface

--- src/Thread.kt
+++ src/Thread.kt
@@ -2,13 +2,20 @@ fun main() {
 
     println("Main Thread starts : ${Thread.currentThread().name}")
 
+    // Creating a new thread using Thread class
+
     val task = Runnable{
         for (i in 1..5){
-            println("Child Thread : $i")
+            println("Running task on  :${Thread.currentThread().name} -> $i")
 
-            Thread.sleep(500)
+            Thread.sleep(500) // pause for 0.5 sec
         }
     }
 
+    val thread = Thread(task)
+    thread.start() // start the thread
+    thread.join() // wait for it to finish
+
+    println("Main thread ends : ${Thread.currentThread().name}")
 
 }
