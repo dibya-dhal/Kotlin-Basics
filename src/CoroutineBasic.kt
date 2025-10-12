105144952dde1149e71c0f218a07c83ace202917
dibya-dhal <dibyadhal32@gmail.com>
2025-10-12 21:48:45 +0530

    Day 46 : Practice basic coroutine with launch in kotlin

--- src/CoroutineBasic.kt
+++ src/CoroutineBasic.kt
@@ -1,8 +1,20 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 
     println("Main starts : ${Thread.currentThread().name}")
 
+
+    launch {
+        for (i in 1..5){
+            println("Coroutine running : $i")
+            delay(500L)
+        }
+    }
+
+    println("Main coroutine on : ${Thread.currentThread().name}")
+    delay(3000)
 
 }
