1ad06939eedb326664ad13127c11fdd405445767
dibya-dhal <dibyadhal32@gmail.com>
2025-10-13 21:54:37 +0530

    Day 47 : Practiced coroutine async and await in kotlin

--- src/CoroutineAsyncAwait.kt
+++ src/CoroutineAsyncAwait.kt
@@ -1,6 +1,26 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 println("Main starts : ${Thread.currentThread().name}")
+
+    val job1 = async {
+        delay(1000)
+        println("Fetching user data..")
+        "User : Rohan"
+    }
+
+    val job2 = async {
+        delay(1500)
+        println("Fetching user score..")
+        95
+    }
+
+
+    val user = job1.await()
+
+
+
 
 }
