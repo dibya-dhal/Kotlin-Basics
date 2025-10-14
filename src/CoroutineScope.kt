d8ff72a309ec41e0731faecc401d8414902987ef
dibya-dhal <dibyadhal32@gmail.com>
2025-10-14 21:59:53 +0530

    Day 48 : Practiced coroutine scopes : GlobalScope, runBlocking, and CoroutineScope

--- src/CoroutineScope.kt
+++ src/CoroutineScope.kt
@@ -1,7 +1,23 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 println("Main starts : ${Thread.currentThread().name}")
+
+    GlobalScope.launch {
+        delay(1000L)
+        println("GlobalScope coroutine : ${Thread.currentThread().name}")
+    }
+
+    launch {
+        delay(500L)
+        println("runBlocking scope : ${Thread.currentThread().name}")
+    }
+    println("Main waiting..")
+    delay(2000L)
+    println("Main ends : ${Thread.currentThread().name}")
 
 
 }
