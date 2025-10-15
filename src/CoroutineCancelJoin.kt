7423cb4c91566549e91321d8b88f08113e875398
dibya-dhal <dibyadhal32@gmail.com>
2025-10-15 22:00:59 +0530

    Day 49 : Practiced coroutine cancel() and join()

--- src/CoroutineCancelJoin.kt
+++ src/CoroutineCancelJoin.kt
@@ -1,7 +1,24 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 println("Main starts : ${Thread.currentThread().name}")
+
+    val job = launch {
+        repeat(5){
+            i ->
+            println("Coroutine working... step ${i + 1}")
+            delay(500L)
+        }
+        println("Coroutine Completed..")
+
+    }
+    delay(1200L)
+    println("Main : Cancelling coroutine after 1..2 seconds")
+    job.cancel()
+    job.join()
 
 
 }
