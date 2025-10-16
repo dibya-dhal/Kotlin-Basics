0aba44ec77133f06aa400ca088c9ca9d279c56a1
dibya-dhal <dibyadhal32@gmail.com>
2025-10-16 22:09:03 +0530

    Day 50 - Practice coroutine exception handling  in kotlin

--- src/CoroutineExceptionHandling.kt
+++ src/CoroutineExceptionHandling.kt
@@ -1,4 +1,6 @@
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
@@ -6,6 +8,15 @@ val handler = CoroutineExceptionHandler { _,
     exception ->
     println("Caught exception : ${exception.message}")
 }
+
+    val job = launch(handler){
+        println("Coroutine started")
+        delay(500L)
+        throw ArithmeticException("Something went wrong!")
+    }
+    job.join()
+    println("Main program continues..")
+
 
 
 }
